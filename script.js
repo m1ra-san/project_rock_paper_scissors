c43918d43c8d5a98743034ba2d26efd984842a78
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-06-25 21:47:07 +0800

    Finalize the playGame function, this complete the full basis of the game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore =0;
+let computerScore=0;
 
 function getComputerChoice() {
     const comp=(Math.floor(Math.random()*10)+1);
@@ -28,7 +30,7 @@ function getHumanChoice() {
 }
 
 
-function playRound(humanChoice, computerChoiceChoice){
+function playRound(humanChoice, computerChoice){
     const map = ["", "Rock", "Paper", "Scissors"];
     console.log("You picked:", map[humanChoice]);
     console.log("Computer picked:", map[computerChoice]);
@@ -40,11 +42,27 @@ function playRound(humanChoice, computerChoiceChoice){
         (humanChoice === 3 && computerChoice === 2) 
     ) {
         console.log("You win!");
+        humanScore++;
     } else {
         console.log("Computer wins!");
+        computerScore++;
     }
+    return [humanScore,computerScore];
 
 }
-let humanChoice= getHumanChoice();
-let computerChoice=getComputerChoice();
-playRound(humanChoice, computerChoice);
+
+function playGame(){
+    let scores=[];
+    for(let i=0; i<=5;i++){
+    let humanChoice= getHumanChoice();
+    let computerChoice= getComputerChoice();
+    scores=playRound(humanChoice, computerChoice);
+    }
+    if(scores[0]>scores[1]){
+        console.log(`Human wins with the score of ${scores[0]}`);
+    }else if(scores[0]<scores[1]) {
+        console.log(`Computer wins with the score of ${scores[1]}`);
+        
+    }else console.log(`Its a tie!! With both score of ${scores[1]}!!`);
+}
+playGame();
