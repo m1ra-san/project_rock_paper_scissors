12806732a083632a010056cb291495e4c2eb3500
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-07-30 19:05:18 +0800

    Add restart panel, finalize design, and clean up code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project_rock_paper_scissors
+
+
+Learned and USE
+-Functions 
+-Arrays
+-DOM Manipulation and Events
+
+This game only utilize front end. Two game modes- pvp and multiplayer and finally game scores or high score between players will be added in the future.
--- script.js
+++ script.js
@@ -39,38 +39,51 @@ function playRound(humanChoice, computerChoice){
     const map = ["", "Rock", "Paper", "Scissors"];
     console.log("You picked:", map[humanChoice]);
     console.log("Computer picked:", map[computerChoice]);
+
+    
     if (humanChoice === computerChoice) {
         console.log("It's a tie!");
+        dialogue.textContent="It's a tie!";
     } else if (
         (humanChoice === 1 && computerChoice === 3) || 
         (humanChoice === 2 && computerChoice === 1) ||
         (humanChoice === 3 && computerChoice === 2) 
     ) {
         console.log("You win!");
+        dialogue.textContent="You win!";
         humanScore++;
     } else {
         console.log("Computer wins!");
+        dialogue.textContent="Computer wins!";
         computerScore++;
     }
 
-    roundsPlayed++
+     //Show and Hide
+    document.querySelector('.dialogue').style.visibility = 'visible';
+    setTimeout(() => {
+        document.querySelector('.dialogue').style.visibility = 'hidden';
+    }, 500);
 
-    if(roundsPlayed>5)endGame()
+    roundsPlayed++
+    if(roundsPlayed>=5)endGame()
 
 }
 
+//---Show Choices/Picks
 function showCompPicked(show){
     const map = ["", "rock", "paper", "scissors"];
     const picked=map[show]
-    const spans = document.querySelectorAll('.computer-side span');
+    const images = document.querySelectorAll('.computer-side img');
     
     // Hide all
-    spans.forEach(span => span.classList.remove('active'));
+    images.forEach(image => {
+        image.classList.remove('active');
+        //delay for animation
+        void image.offsetWidth;
+    });
     
     // Show selected
     const selected = document.querySelector(`#comp-${picked}`);
-    console.log(selected)
-    console.log(picked)
     if (selected) {
       selected.classList.add('active');
     }
@@ -80,51 +93,77 @@ function showCompPicked(show){
 function showPlayerPicked(show){
     const map = ["", "rock", "paper", "scissors"];
     const picked=map[show]
-    const spans = document.querySelectorAll('.player-side span');
+    const images = document.querySelectorAll('.player-side img');
     
     // Hide all
-    spans.forEach(span => span.classList.remove('active'));
-    
+    images.forEach(image=> {
+        image.classList.remove('active');
+        //delay for animation
+        void image.offsetWidth;
+    });
     // Show selected
     const selected = document.querySelector(`#user-${picked}`);
-    console.log(selected)
-    console.log(picked)
     if (selected) {
       selected.classList.add('active');
     }
   
 }
 
-function endGame(){
 
+//Show the Resart Panel
+function endGame(){
+    document.querySelector('.restart-container').classList.add('visible');
     if(humanScore>computerScore){
-        console.log(`Human wins with the score of ${humanScore}`);
+        console.log(`You wins with the score of ${humanScore}`);
+        endDialogue.textContent="Human wins with the score of";
+         displayScore.textContent=`${humanScore}`;
     }else if(humanScore<computerScore) {
         console.log(`Computer wins with the score of ${computerScore}`);
-        
-    }else console.log(`Its a tie!! With both score of ${computerScore}!!`);
-    // restart()
+        endDialogue.textContent="Computer wins with the score of";
+        displayScore.textContent=`${computerScore}`;
+    }else{
+        console.log(`Its a tie!! With both score of ${computerScore}!!`);
+        endDialogue.textContent="Its a tie!! With both score of";
+        displayScore.textContent=`${computerScore}`;
+    } 
     
 }
 
-function restart(){
+//Restrat the Game
+const restartScore=()=>{
+    document.querySelector('.restart-container').classList.remove('visible');
     humanScore = 0;
     computerScore = 0;
     roundsPlayed = 0;
-    const spans = document.querySelectorAll('.computer-side span');
-    spans.forEach(span => span.classList.remove('active'));
-    spans.item(0).classList.add('active');
-}
+    backToDefault()
+};
 
+const backToDefault=()=>{
+    const both=[".computer-side",".player-side"]
+    both.forEach(selector=>{
+        const images = document.querySelectorAll(`${selector} img`)
+        images.forEach(span => span.classList.remove('active'));
+        images.item(0).classList.add('active');
+    });
+};
+
+
+//Global Event Listener
 function globalEventListener(type,selector,callback){
     document.addEventListener(type, e=>{
         if(e.target.matches(selector)) callback(e)
     })
 }
 
+
+
 let humanScore =0;
 let computerScore=0;
 let roundsPlayed = 0;
 
+
+const dialogue= document.querySelector('.dialogue')
+const endDialogue=document.querySelector('.restart-panel p')
+const displayScore=document.querySelector('.restart-panel div')
 globalEventListener("click",".game-buttons button",getHumanChoice);
-globalEventListener("click",".restart-panel button",restart);
+globalEventListener("click",'#btnrestart',restartScore);
